a350e774ec83a9ecd9321493646523703648fe9d
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-01-18 07:41:56 +0100

    fix

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -3,7 +3,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
     "splmaps ",
-    # utils::packageDescription("splmaps")$Version,
+    utils::packageDescription("splmaps")$Version,
     "\n",
     "https://docs.sykdomspulsen.no/splmaps"
   ))
