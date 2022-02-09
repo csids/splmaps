adfd3a0b099851916489ebb1ee47293b838b92a7
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 12:09:20 +0100

    onAttach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,8 +1,15 @@
 #' @import data.table
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("splmaps", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
+
   packageStartupMessage(paste0(
     "splmaps ",
-    utils::packageDescription("splmaps")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/splmaps"
   ))
